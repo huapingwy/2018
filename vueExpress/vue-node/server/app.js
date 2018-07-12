284c8419657da10a368bea91bc5a18f6192cf2a9
Huaping <1019227223@qq.com>
2018-07-12 18:07:09 +0800

    11111

--- vueExpress/vue-node/server/app.js
+++ vueExpress/vue-node/server/app.js
@@ -3,6 +3,8 @@ const fs = require('fs')
 const path = require('path')
 const bodyParser = require('body-parser')
 
+const shopModel = require('./open-shop')
+
 const app = express()
 
 app.use(bodyParser.json())
@@ -13,6 +15,31 @@ app.use(express.static(path.resolve(__dirname, '../dist')))
 app.get('*', (req, res) => {
   const html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8')
   res.send(html)
+})
+
+app.post('/create/shop', (req, res) => {
+  console.log(req.body)
+
+  const newshop = new shopModel({
+    name: req.body.name,
+    logo: req.body.logo
+  })
+
+  newshop.save((err) => {
+    if (err) {
+      res.json({
+        code: 400032,
+        data: null,
+        message: '提交失败'
+      })
+    } else {
+      res.json({
+        code: 2000,
+        data: req.body,
+        messgae: 'success'
+      })
+    }
+  })
 })
 
 app.listen(8081, () => {
